fb2ea66a145c4f21ca7b99b2eda6ae3ce2af38ea
Aayush kumar <aayush3751@gmail.com>
2026-09-05 22:58:57 +0530

    Fix backend start script and PORT environment variable for Render

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 var app = express();
 
 app.use(cors());
-const port = 5000
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello patlu!')
